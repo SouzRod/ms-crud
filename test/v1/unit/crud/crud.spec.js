f826042917c640156df6b74ebb956a8da2d15e42
Rodrigo Souza <rdrg.rcs1997@gmail.com>
2022-02-17 15:16:33 -0300

    unit tests

--- test/v1/unit/crud/crud.spec.js
+++ test/v1/unit/crud/crud.spec.js
@@ -17,7 +17,8 @@ describe('Unit tests for crud adapter', () => {
       payload: {},
       repository: {
         usersCollection: {
-          findOne: objSinon.spy(() => Promise.resolve({ id: '123456', name: 'Leo Santander' })),
+          findMany: objSinon.spy(() => Promise.resolve({ id: '123456', name: 'Leo Santander' })),
+          insert: objSinon.spy(() => Promise.resolve({ id: '123456', name: 'Leo Santander' })),
         },
       },
       onSuccess: objSinon.spy(onSuccess => onSuccess),
@@ -27,27 +28,45 @@ describe('Unit tests for crud adapter', () => {
 
   context('smoke tests', () => {
     it('should have a get crudWrapper created', async () => {
-      const { get } = crudWrapper(mockDependencies);
+      const { get, post } = crudWrapper(mockDependencies);
       expect(get).to.be.a('function');
+      expect(post).to.be.a('function');
     });
   });
 
-  context('Get ok', () => {
+  context('ok', () => {
     it('should return object success when get method', async () => {
       await crudWrapper(mockDependencies).get(mockDependencies);
       const result = mockDependencies.onSuccess.returnValues[0];
       expect(result.name).to.be.equal('Leo Santander');
     });
+
+    it('should return object success when post method', async () => {
+      await crudWrapper(mockDependencies).post(mockDependencies);
+      const result = mockDependencies.onSuccess.returnValues[0];
+      expect(result.name).to.be.equal('Leo Santander');
+    });
   });
 
-  context('Get error', () => {
-    it('should return error when database returns error', async () => {
+  context('error', () => {
+    it('get should return error when database returns error', async () => {
       mockDependencies.repository = {
         usersCollection: {
           findOne: objSinon.spy(() => Promise.reject(new Error('Error'))),
         },
       };
       const result = await crudWrapper(mockDependencies).get(mockDependencies);
+      expect(result).to.have.property('stack');
+      expect(result).to.have.property('message');
+    });
+
+    it('post should return error when database returns error', async () => {
+      mockDependencies.repository = {
+        usersCollection: {
+          findOne: objSinon.spy(() => Promise.reject(new Error('Error'))),
+        },
+      };
+      const result = await crudWrapper(mockDependencies).post(mockDependencies);
       expect(result).to.have.property('stack');
       expect(result).to.have.property('message');
     });
